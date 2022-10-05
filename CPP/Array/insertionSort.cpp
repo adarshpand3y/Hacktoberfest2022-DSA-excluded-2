0d14de0a4e5e1493c5b8073d3b8d8b17637c042d
Lokesh-malviya <malviyal@rknec.edu>
2022-10-06 00:02:56 +0530

    insertion sort cpp

--- CPP/Array/insertionSort.cpp
+++ CPP/Array/insertionSort.cpp
@@ -1,38 +1,27 @@
-#include <bits/stdc++.h>
+//Insertion Sort Algorithm
+
+#include<bits/stdc++.h>
 using namespace std;
 
-void insertionSort(int arr[], int n)
-{
-    int i, key, j;
-    for (i = 1; i < n; i++)
-    {
-        key = arr[i];
-        j = i - 1;
-
-        while (j >= 0 && arr[j] > key)
-        {
-            arr[j + 1] = arr[j];
-            j = j - 1;
+void insertionsort(int a[], int n){
+    for(int i=1; i<n; i++){
+        int value=a[i];
+        int blank=i;
+        while(blank>0 && (a[blank-1]>value)){
+            a[blank]=a[blank-1];
+            blank--;
         }
-        arr[j + 1] = key;
+        a[blank]=value;
     }
+    for(int i=0; i<n; i++)
+    cout<<a[i]<<" ";
 }
 
-void printArray(int arr[], int n)
-{
-    int i;
-    for (i = 0; i < n; i++)
-        cout << arr[i] << " ";
-    cout << endl;
-}
-
-int main()
-{
-    int arr[] = {12, 4, 6, 2, 44, 32};
-    int N = sizeof(arr) / sizeof(arr[0]);
-
-    insertionSort(arr, N);
-    printArray(arr, N);
-
-    return 0;
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++)
+    cin>>a[i];
+    insertionsort(a,n); 
 }
