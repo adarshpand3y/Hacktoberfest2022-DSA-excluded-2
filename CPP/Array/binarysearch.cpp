f917587831cdfd708c77d6d3f4f40a9f0ed39dbb
SidAsif <asif050md@gmail.com>
2022-10-05 15:25:03 +0530

    Modified binarysearch.cpp

--- CPP/Array/binarysearch.cpp
+++ CPP/Array/binarysearch.cpp
@@ -1,41 +1,48 @@
+// for binary_Search----> elements should be in monoatomic function(always in increasing or decresing order)
+// complexity = O(log N)
 #include <iostream>
-using namespace std;
+using namespace std;   
 
-int BinarySearch(int n, int search, int arr[])
+int binarySearch(int arr[], int size, int key)
 {
-    int s = 0;
-    int e = n;
-    int mid = (s + e) / 2;
-    while (s <= e)
+    int start = 0;
+    int end = size - 1;
+   // int mid = (start + end) / 2;  // mid=(s+e)/2 = s + (e-s)/2--->using this bcz when bigger value come they give error due to overflow range of int
+                                  // = s + e/2 - s/2
+                                  // = s/2 + e/2 = (s + e)/2
+    
+    int mid = start + (end - start)/2;
+    while (start <= end)
     {
-        if (arr[mid] == search)
+        if (arr[mid] == key)
         {
-            return mid;
+            return 1;
         }
-        else if (arr[mid] < search)
+        if (key > arr[mid])
         {
-            s = mid + 1;
+            start = mid + 1;
         }
-        else{
-            e=mid-1;
+        else
+        {
+            end = mid - 1;
         }
+        mid = start + (end - start)/2;
     }
-    return -1;
+    return 0;
 }
 
 int main()
 {
-    int n;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-
-    int number;
-    cin >> number;
-    cout << BinarySearch(n, number, arr);
-
+    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+    cout << "Enter the element to search for: " << endl;
+    int key;
+    cin >> key;
+  int found=binarySearch(arr,10,key);
+  if(found){
+      cout<<"Element is present"<<endl;
+  }
+  else{
+      cout<<"Element is absent"<<endl;
+  }
     return 0;
 }
