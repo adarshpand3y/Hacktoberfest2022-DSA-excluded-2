ef947c486644c6d839a0d9ed589c4ffd3befedd3
Tanuj Kalonia <tanujkalonia2002@gmail.com>
2022-10-07 10:04:26 +0530

    Added a question (Construct bst from preorder traversal)

---
+++ CPP/Binary Search Tree/construct_BST_from_postorder.cpp
@@ -0,0 +1,90 @@
+
+/*
+    Contributed by Tanuj Kalonia
+
+    Problem Statement :
+    - You are given a postorder traversal arrya of a binary tree,
+    - You have to create a binary Search Tree using the postorder traversal.
+
+    Input :
+    - 1st line will contain "t" test cases
+    - Then immideate to above line contains an integer "n" and an array of array size n.
+
+    Output : single line preorder traveral of resulting binary tree.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x)
+    {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+Node *constructTree(int[], int);
+
+void printInorder(Node *node)
+{
+    if (node == NULL)
+        return;
+    printInorder(node->left);
+    printf("%d ", node->data);
+    printInorder(node->right);
+}
+
+int main()
+{
+    int t, n;
+    scanf("%d", &t);
+    while (t--)
+    {
+        scanf("%d", &n);
+        int post[n];
+        for (int i = 0; i < n; i++)
+            scanf("%d", &post[i]);
+
+        Node *root = constructTree(post, n);
+
+        printInorder(root);
+        printf("\n");
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+/*struct Node
+{
+    int data;
+    Node *left, *right;
+};*/
+
+Node *convert(int post[], int left, int right, int &i, int size)
+{
+    if (left > right)
+        return nullptr;
+    if (i == size)
+        return nullptr;
+    if (post[i] < left or post[i] > right)
+        return nullptr;
+
+    int x = post[i];
+    Node *root = new Node(post[i--]);
+    root->right = convert(post, x + 1, right, i, size);
+    root->left = convert(post, left, x - 1, i, size);
+    return root;
+}
+Node *constructTree(int post[], int size)
+{
+
+    int i = size - 1;
+    return convert(post, INT_MIN, INT_MAX, i, size);
+}
