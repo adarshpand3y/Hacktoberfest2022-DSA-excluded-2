4110dd9c6070b801a1064417c7ada8b2070c6661
MrTypeError <sanugta8@gmail.com>
2022-10-07 22:16:28 +0530

    To sum Problem is add

---
+++ Python/Array/toSumProblem.py
@@ -0,0 +1,16 @@
+
+# One of the very popular QUESTION
+
+
+def twoSum(n, nums, target):
+    # Time Complexity = O(n**2) 
+    # Time Limit will exceed with certain cases
+    for i in range(len(nums)): #for the rows
+        for j in range(len(nums)): #respective columns
+            if nums[i]+nums[j] == target and i!=j:
+                    return [min(i,j),max(i,j)]
+
+
+
+# Function call is done here 
+print(twoSum(6,[1,2,3,4,5,6,23],9))
