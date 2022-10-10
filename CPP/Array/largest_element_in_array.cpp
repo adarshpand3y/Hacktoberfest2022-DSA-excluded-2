3a5a33cb314feab4bc2c88bc155756112d4208d4
Shreyoshi Chakraborty <shreyoshi5000@gmail.com>
2022-10-10 23:45:57 +0530

    Modified largest_element_in_array.cpp to find without sorting

--- CPP/Array/largest_element_in_array.cpp
+++ CPP/Array/largest_element_in_array.cpp
@@ -5,13 +5,27 @@ int sortArr(vector<int>& arr) {
     sort(arr.begin(),arr.end());
     return arr[arr.size()-1];
 }
+
+int largest_ele(vector<int>& arr){
+    int largest = 0;
+    for(int i=0; i<arr.size(); i++)
+    {
+        if(arr[i]>arr[largest])
+        {
+            largest = i;
+        }
+    }
+    return arr[largest];
+}
  
 int main() {
     vector<int> arr1 = {2,5,1,3,0};
     vector<int> arr2 = {8,10,5,7,9};
    
     cout<<"The Largest element in the array is: "<<sortArr(arr1)<<endl;
-    cout<<"The Largest element in the array is: "<<sortArr(arr2);
-   
+    cout<<"The Largest element in the array is: "<<sortArr(arr2)<<endl;
+
+    cout<<"The Largest element in the array without sorting the array is: "<<largest_ele(arr1)<<endl;
+    cout<<"The Largest element in the array without sorting the array is: "<<largest_ele(arr2);
     return 0;
 }
