16285bf423e3004d9354bf3bdbb26783140359dd
jainmalaykumar <111597042+jainmalaykumar@users.noreply.github.com>
2022-10-12 17:58:01 +0530

    Updated BMI_Calculator.cpp
    Added Age Factor and Obesity Range.

--- BMI_Calculator.cpp
+++ BMI_Calculator.cpp
@@ -3,14 +3,25 @@ using namespace std;
 
 int main()
 {
-  float bmi,weight,height;
+  float bmi,weight,height,age;
   cout<<"Please Enter your weight in Pounds : ";
   cin>>weight;
   cout<<"Please Enter your Height in Inches : ";
   cin>>height;
+  cout<<"Enter your Age : ";
+  while(true){
+    cin>>age;
+    if(age>=20)
+      break;
+    else
+      cout<<"This Application is only for Adults(20 years old or elder)"<<endl;
+      cout<<"Please re-enter your your age to get accurate results!!!");
+  }
   bmi=(weight*703)/(height*height);
   cout<<"Your BMI is : "<<bmi<<endl;
-  if(bmi>25)
+  if(bmi>=30)
+    cout<<"Your BMI value shows that you are obese.";
+  else if(bmi>25 && bmi<30)
     cout<<"Your BMI value shows that you are overweight.";
   else if(bmi<25 && bmi>18.5)
     cout<<"Your weight is normal.";
