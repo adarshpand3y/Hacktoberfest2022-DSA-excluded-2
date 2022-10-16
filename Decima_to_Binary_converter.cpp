3aa5336fee73355e34351f5b84d791af3c287430
Soumyadip Debnath <dsoumyadip202@gmail.com>
2022-10-16 21:40:42 +0530

    Decimal to Binary

--- Decima_to_Binary_converter.cpp
+++ Decima_to_Binary_converter.cpp
@@ -1,18 +1,27 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-int n,sum=0,i=1;
-cout<<"Enter: ";
-cin>>n;
+int main()
+{
+    int a, b=0;
+    int n;
+    cout<<"Enter a number in Decimal Number System"<<endl;
+    cin>>n;
 
-while(n>0){
-    sum+=i*(n%10);
-    n/=10;
-    i*=2;
-}
-
-cout<<"Decimal Equivalent: "<<sum;
-
+    while(n>0)
+    {
+        a= n%2;
+        b= (b*10)+a;
+        n= n/2;
+    }
+   int p, q=0;
+   while(b>0)
+   {
+       p= b%10;
+       q= (q*10)+p;
+       b=b/10;
+   }
+    cout<<"The Binary equivalent is: "<< q;
+    
     return 0;
 }
